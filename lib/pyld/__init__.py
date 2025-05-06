118f69e694d9f866b862e95af1cef5d38465d5eb
bosteen@getty.edu <bosteen@getty.edu>
2025-05-05 17:01:31 -0700

    Updating to use pyproject.toml

--- lib/pyld/__init__.py
+++ lib/pyld/__init__.py
@@ -1,5 +1,9 @@
 """ The PyLD module is used to process JSON-LD. """
+
 from . import jsonld
 from .context_resolver import ContextResolver
 
-__all__ = ['jsonld', 'ContextResolver']
+
+__version__ = "2.0.5"
+
+__all__ = ["jsonld", "ContextResolver"]
