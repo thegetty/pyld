926c05d760354344aa9ee03b872fd3c74313b1e5
bosteen <bosteen@getty.edu>
2026-08-19 10:30:22 -0700

    Adding threadlocks around the read/write of the LRUCache (port of d6a40b2 to 3.2.0)

--- lib/pyld/resolved_context.py
+++ lib/pyld/resolved_context.py
@@ -9,6 +9,7 @@ Representation for a resolved Context.
 """
 
 from cachetools import LRUCache
+import threading
 
 MAX_ACTIVE_CONTEXTS = 10
 
@@ -26,15 +27,18 @@ class ResolvedContext:
         # processor-specific RDF parsers
         self.document = document
         self.cache = LRUCache(maxsize=MAX_ACTIVE_CONTEXTS)
+        self.lock = threading.Lock()
 
     def get_processed(self, active_ctx):
         """
         Returns any processed context for this resolved context relative to an active context.
         """
-        return self.cache.get(active_ctx['_uuid'])
+        with self.lock:
+            return self.cache.get(active_ctx["_uuid"])
 
     def set_processed(self, active_ctx, processed_ctx):
         """
         Sets any processed context for this resolved context relative to an active context.
         """
-        self.cache[active_ctx['_uuid']] = processed_ctx
+        with self.lock:
+            self.cache[active_ctx["_uuid"]] = processed_ctx
